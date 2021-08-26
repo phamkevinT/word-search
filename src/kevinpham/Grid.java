c47b661981a17fb66ee04a46bb34cbbe123aa7ac
Kevin Pham <phambamkevin@gmail.com>
2021-08-26 14:10:08 -0700

    Support multiple directions of word placement

--- src/kevinpham/Grid.java
+++ src/kevinpham/Grid.java
@@ -1,6 +1,7 @@
 package kevinpham;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -12,6 +13,11 @@ public class Grid {
     // List of possible coordinates within grid
     private List<Coordinate> coordinates = new ArrayList<>();
 
+    private enum Direction {
+        HORIZONTAL,
+        VERTICAL,
+        DIAGONAL
+    }
 
     private class Coordinate {
         int x;
@@ -51,22 +57,38 @@ public class Grid {
      * @param words the list of words to find
      */
     public void fillGrid(List<String> words) {
+        // Randomize and shuffle the coordinates in the list
+        Collections.shuffle(coordinates);
 
         for (String word : words) {
-            // Randomize and shuffle the coordinates in the list
-            Collections.shuffle(coordinates);
-
             // Loop through each coordinate and see which one could fit word
             for (Coordinate coordinate : coordinates) {
                 int x = coordinate.x;
                 int y = coordinate.y;
+                Direction selectedDirection = getDirectionForFit(word, coordinate);
 
-                // Checking if the word fits in grid
-                if (doesFit(word, coordinate)) {
-                    // Convert word to charArray and for each character insert into grid
-                    for (char c : word.toCharArray()) {
-                        contents[x][y++] = c;
+                if (selectedDirection != null) {
+
+                    // Place word based on valid direction
+                    switch (selectedDirection) {
+                        case HORIZONTAL:
+                            // Convert word to charArray and for each character insert into grid
+                            for (char c : word.toCharArray()) {
+                                contents[x][y++] = c;
+                            }
+                            break;
+                        case VERTICAL:
+                            for (char c : word.toCharArray()) {
+                                contents[x++][y] = c;
+                            }
+                            break;
+                        case DIAGONAL:
+                            for (char c : word.toCharArray()) {
+                                contents[x++][y++] = c;
+                            }
+                            break;
                     }
+                    // After we fit a word, move onto the next word
                     break;
                 }
             }
@@ -89,25 +111,57 @@ public class Grid {
 
 
     /**
-     * Check if word fits within grid considering coordinates given
+     * Check if word fits within grid based on random direction
      *
      * @param word       the word
-     * @param coordinate the coordinates
-     * @return true if word fits, otherwise false
+     * @param coordinate the coordinate
+     * @return Valid word placement direction
      */
-    private boolean doesFit(String word, Coordinate coordinate) {
-        // Check if word size fits grid
-        if (coordinate.y + word.length() < gridSize) {
-            // Check if all proceeding elements are underscore to prevent word overlap
-            for (int i = 0; i < word.length(); i++) {
-                if (contents[coordinate.x][coordinate.y + i] != '_') {
-                    return false;
-                }
-                return true;
+    private Direction getDirectionForFit(String word, Coordinate coordinate) {
+
+        List<Direction> directions = Arrays.asList(Direction.values());
+        Collections.shuffle(directions);
+        for (Direction direction : directions) {
+            if (doesFit(word, coordinate, direction)) {
+                return direction;
             }
         }
-        return false;
+        return null;
     }
 
+
+    /**
+     * Check if word fits in grid given word, coordinate, and direction
+     *
+     * @param word       the word
+     * @param coordinate the coordinate
+     * @param direction  random direction
+     * @return true if fits, otherwise false
+     */
+    private boolean doesFit(String word, Coordinate coordinate, Direction direction) {
+        int wordLength = word.length();
+
+        switch (direction) {
+            case HORIZONTAL:
+                if (coordinate.y + wordLength > gridSize) return false;
+                for (int i = 0; i < wordLength; i++) {
+                    if (contents[coordinate.x][coordinate.y + i] != '_') return false;
+                }
+                break;
+            case VERTICAL:
+                if (coordinate.x + wordLength > gridSize) return false;
+                for (int i = 0; i < wordLength; i++) {
+                    if (contents[coordinate.x + i][coordinate.y] != '_') return false;
+                }
+                break;
+            case DIAGONAL:
+                if (coordinate.x + wordLength > gridSize || coordinate.y + wordLength > gridSize) return false;
+                for (int i = 0; i < wordLength; i++) {
+                    if (contents[coordinate.x + i][coordinate.y + i] != '_') return false;
+                }
+                break;
+        }
+        return true;
+    }
 
 }
