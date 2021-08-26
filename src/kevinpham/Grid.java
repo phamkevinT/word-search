1940d3bb98eafa608b49f63c3cc9b77d8cb71858
Kevin Pham <phambamkevin@gmail.com>
2021-08-26 13:20:22 -0700

    Added boundary check for words

--- src/kevinpham/Grid.java
+++ src/kevinpham/Grid.java
@@ -1,23 +1,44 @@
 package kevinpham;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class Grid {
 
     private int gridSize;
-
     // Character grid as 2D Array
     private char[][] contents;
+    // List of possible coordinates within grid
+    private List<Coordinate> coordinates = new ArrayList<>();
 
+
+    private class Coordinate {
+        int x;
+        int y;
+
+        Coordinate(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+
+    /**
+     * Initializes grid with underscores based on grid size
+     * Adds all possible coordinates to list of coordinates
+     *
+     * @param gridSize the grid size
+     */
     public Grid(int gridSize) {
-
         this.gridSize = gridSize;
         contents = new char[gridSize][gridSize];
 
         // Initialize grid with underscores
         for (int i = 0; i < gridSize; i++) {
             for (int j = 0; j < gridSize; j++) {
+                // Add all possible coordinates into list of coordinates
+                coordinates.add(new Coordinate(i, j));
                 contents[i][j] = '_';
             }
         }
@@ -25,26 +46,29 @@ public class Grid {
 
 
     /**
-     * Populate grid with words to find
+     * Populate grid with words
      *
      * @param words the list of words to find
      */
     public void fillGrid(List<String> words) {
 
         for (String word : words) {
+            // Randomize and shuffle the coordinates in the list
+            Collections.shuffle(coordinates);
 
-            // Get a random number between 0 and GRID_SIZE for x-coord and y-coord starting point for word
-            int x = ThreadLocalRandom.current().nextInt(0, gridSize);
-            int y = ThreadLocalRandom.current().nextInt(0, gridSize);
+            // Loop through each coordinate and see which one could fit word
+            for (Coordinate coordinate : coordinates) {
+                int x = coordinate.x;
+                int y = coordinate.y;
 
-            // Checking if a word is too large to fit in grid
-            if (y + word.length() >= gridSize) {
-                continue;
-            }
-
-            // Convert word to charArray and for each character insert into grid
-            for (char c : word.toCharArray()) {
-                contents[x][y++] = c;
+                // Checking if the word fits in grid
+                if (y + word.length() < gridSize) {
+                    // Convert word to charArray and for each character insert into grid
+                    for (char c : word.toCharArray()) {
+                        contents[x][y++] = c;
+                    }
+                    break;
+                }
             }
         }
     }
